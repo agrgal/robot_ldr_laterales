5ccfdb98c4ec784e9433a5126d581643d7e7a6dc
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2022-10-23 12:28:51 +0200

    Cambios definitivos

--- main.ts
+++ main.ts
@@ -1,17 +1,18 @@
 let LDRIzq = 0
 let LDRDRCH = 0
-let maxLDRDRCH = pins.analogReadPin(AnalogPin.P1)
-let maxLDRIZq = pins.analogReadPin(AnalogPin.P2)
+basic.pause(1000)
+let maxLDRDRCH = pins.analogReadPin(AnalogPin.P0)
+let maxLDRIZq = pins.analogReadPin(AnalogPin.P1)
 basic.pause(500)
 let potIZQ = 20
 let potDRCH = 20
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    LDRDRCH = pins.analogReadPin(AnalogPin.P1)
-    LDRIzq = pins.analogReadPin(AnalogPin.P2)
-    potIZQ = Math.map(LDRIzq, maxLDRIZq, 2, 20, 100)
-    potDRCH = Math.map(LDRDRCH, maxLDRDRCH, 2, 20, 100)
+    LDRDRCH = pins.analogReadPin(AnalogPin.P0)
+    LDRIzq = pins.analogReadPin(AnalogPin.P1)
+    potIZQ = Math.map(LDRIzq, maxLDRIZq, 2, 20, 50)
+    potDRCH = Math.map(LDRDRCH, maxLDRDRCH, 2, 20, 50)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, potIZQ)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, potDRCH)
-    basic.pause(10)
+    basic.pause(25)
 })
