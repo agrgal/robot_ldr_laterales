bc60bcf0d00b4f44b680d8f00e06f6b68f1e3d01
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2022-10-23 11:16:13 +0200

    subsanar error

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
 let LDRIzq = 0
 let LDRDRCH = 0
 let maxLDRDRCH = pins.analogReadPin(AnalogPin.P1)
-maxLDRDRCH = pins.analogReadPin(AnalogPin.P2)
+let maxLDRIZq = pins.analogReadPin(AnalogPin.P2)
 basic.pause(500)
 let potIZQ = 20
 let potDRCH = 20
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    let maxLDRIZq = 0
     LDRDRCH = pins.analogReadPin(AnalogPin.P1)
     LDRIzq = pins.analogReadPin(AnalogPin.P2)
     potIZQ = Math.map(LDRIzq, maxLDRIZq, 2, 20, 100)
